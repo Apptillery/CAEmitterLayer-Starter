434c7422b59737f2ff73f7cb2e9f6c25fbada887
Alex Gibson <oakmonttech@gmail.com>
2017-05-11 15:26:19 -0400

    Starter

---
+++ StarsAnimation/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  StarsAnimation
+//
+//  Created by Alex Gibson on 5/11/17.
+//  Copyright © 2017 Apptillery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func buttonDidPressDown(_ sender: UIButton) {
+
+        
+    }
+
+    
+
+}
+
